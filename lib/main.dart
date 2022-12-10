782cfc7ab7e3103b7207f3295d38d91b9db08fd3
adaltoPrado <adaltoprado@proton.me>
2022-12-10 19:05:39 -0300

    Rounded 'Calculate' button

--- lib/main.dart
+++ lib/main.dart
@@ -31,41 +31,55 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
   TextEditingController pesoController = TextEditingController(text: '');
   TextEditingController alturaController = TextEditingController(text: '');
 
+  double? imc;
+  //String classificacao;
+  //Color corResultado;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      // ignore: sized_box_for_whitespace
       body: Container(
         width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              width: 300,
-              height: 300,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(150),
-                border: Border.all(
-                  width: 10,
-                  color: Colors.green,
-                ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    '24.22',
-                    style: TextStyle(fontSize: 42, color: Colors.green),
+            imc == null
+                ? Text(
+                    'Adicione valores de peso e altura \n para calcular seu IMC',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontStyle: FontStyle.italic,
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+                : Container(
+                    width: 300,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(150),
+                      border: Border.all(
+                        width: 10,
+                        color: Colors.green,
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          '24.22',
+                          style: TextStyle(fontSize: 42, color: Colors.green),
+                        ),
+                        SizedBox(height: 12),
+                        Text(
+                          'Peso Normal',
+                          style: TextStyle(fontSize: 20, color: Colors.green),
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 12),
-                  Text(
-                    'Peso Normal',
-                    style: TextStyle(fontSize: 20, color: Colors.green),
-                  ),
-                ],
-              ),
-            ),
             SizedBox(height: 40),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -73,14 +87,16 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
                 Column(
                   children: [
                     Text('Seu peso'),
-                    SizedBox(height: 6),
+                    SizedBox(height: 8),
                     Container(
                       width: 75,
                       child: TextField(
                         controller: pesoController,
                         decoration: InputDecoration(
                           border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+                            borderRadius: BorderRadius.circular(
+                              20,
+                            ),
                           ),
                           suffixText: 'Kg',
                         ),
@@ -93,14 +109,16 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
                 Column(
                   children: [
                     Text('Sua altura'),
-                    SizedBox(height: 6),
+                    SizedBox(height: 8),
                     Container(
                       width: 75,
                       child: TextField(
                         controller: alturaController,
                         decoration: InputDecoration(
                           border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+                            borderRadius: BorderRadius.circular(
+                              20,
+                            ),
                           ),
                           suffixText: 'M',
                         ),
@@ -116,11 +134,22 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
               width: 200,
               height: 60,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  print(pesoController.text);
+                  print(alturaController.text);
+                },
                 child: Text(
                   'Calcular',
                   style: TextStyle(
                     color: Colors.white,
+                  ),
+                ),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                      side: BorderSide(color: Colors.black),
+                    ),
                   ),
                 ),
               ),
