e5e54fdc063d78cc5bf07ec72b28da241bf52b7f
adaltoPrado <adaltoprado@proton.me>
2022-12-10 22:59:36 -0300

    Output text change

--- lib/main.dart
+++ lib/main.dart
@@ -172,13 +172,13 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
     } else if (imc > 18.5 && imc <= 24.9) {
       return 'Eutrofia (Peso Adequado)';
     } else if (imc >= 25 && imc <= 29.9) {
-      return 'Você está com Sobrepeso';
+      return 'Sobrepeso';
     } else if (imc > 30 && imc <= 34.9) {
-      return 'Você está com Obesidade Grau I';
+      return 'Obesidade Grau I';
     } else if (imc >= 35 && imc <= 39.9) {
-      return 'Você está com Obesidade Grau II';
+      return 'Obesidade Grau II';
     } else if (imc >= 40) {
-      return 'Você está com Obesidade Extrema';
+      return 'Obesidade Extrema';
     }
     throw '';
   }
